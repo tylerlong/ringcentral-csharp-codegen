743d12cc4698f9aa432e9f1b38520745e94fb95c
Tyler Long <tyler4long@gmail.com>
2016-04-14 19:12:44 +0800

    Add internal spec

--- fabfile.py
+++ fabfile.py
@@ -9,5 +9,9 @@ def advanced():
     local('swagger-codegen generate -i specs/advanced.json -l csharp -o ./tmp/')
 
 
+def internal():
+    local('swagger-codegen generate -i specs/internal.json -l csharp -o ./tmp/')
+
+
 def js():
     local('swagger-codegen generate -i specs/basic.json -l javascript -o ./tmp/')
